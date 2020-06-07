4b00e3114c50d7deafe90c04fec91fd81c5ff72f
Alexandru Iacoboae <alexiacoboae@gmail.com>
2020-06-07 11:54:21 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRank_Problem_Solving
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //fizz buz
+
+            // Result.fizzBuzz(15);
+
+            // Plus Minus problem
+
+            //Console.WriteLine("Give me the array separated by spaces \" \" ");
+
+            //int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp));
+
+            //Result.PlusMinus(arr);
+
+            //stair cases problem
+
+            //Result.StairCase(10);
+
+            //Mini-Max Sum
+            
+
+
+        }
+    }
+}
---
+++ Result.cs
@@ -0,0 +1,68 @@
+﻿using System.Linq;
+using System;
+
+namespace HackerRank_Problem_Solving
+{
+
+    public class Result
+    {
+
+        public static void fizzBuzz(int n)
+        {
+
+
+            for (int i = 1; i <= n; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    System.Console.WriteLine("FizzBuzz");
+                }
+
+                else if (i % 3 == 0)
+                {
+                    System.Console.WriteLine("Fizz");
+                }
+
+                else if (i % 5 == 0)
+                {
+                    System.Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    System.Console.WriteLine(i);
+                }
+            }
+
+        }
+
+        public static void PlusMinus(int[] arr)
+        {
+            int n = arr.Length;
+            float result;
+
+            var a = arr.Count(val => val > 0);
+            result = (float)a / n;
+            Console.WriteLine(result);
+
+            var b = arr.Count(val => val < 0);
+            result = (float)b / n;
+            Console.WriteLine(result);
+
+            var c = arr.Count(val => val == 0);
+            result = (float)c / n;
+            Console.WriteLine(result);
+
+        }
+
+        public static void StairCase(int v)
+        {
+            for (int i = 0; i < v; i++)
+            {
+                Console.WriteLine(new string('#',i+1).PadLeft(v, ' '));
+            }
+            
+        }
+
+    }
+
+}
